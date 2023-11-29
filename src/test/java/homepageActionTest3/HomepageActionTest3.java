c27ec101a77757752c3f4c08e2ea817a2bb46cdd
Neamul Khan <neamulkhan@yahoo.com>
2023-11-28 20:36:16 -0500

    login feature added

--- src/test/java/homepageActionTest3/HomepageActionTest3.java
+++ src/test/java/homepageActionTest3/HomepageActionTest3.java
@@ -1,5 +1,7 @@
 package homepageActionTest3;
 
+import org.testng.Assert;
+
 import homepageLocatorTest3.HomepageLocatorTest3;
 import utilityTest3.BaseClassTest3;
 
@@ -21,6 +23,32 @@ public class HomepageActionTest3 extends BaseClassTest3{
 		Thread.sleep(2000);
 		homepageLocatorTest3.signupButton.click();
 		
+	}
+	
+	public void click_loginLink() throws Exception {
+		
+		Thread.sleep(3000);
+		
+		homepageLocatorTest3.login.click();
+				
+	}
+	
+	public void enter_username_password_and_click_login_button() throws Exception {
+		
+		homepageLocatorTest3.login_username.sendKeys("raisa");
+		homepageLocatorTest3.login_password.sendKeys("123456");
+		Thread.sleep(2000);
+		
+		homepageLocatorTest3.loginButton.click();
+		Thread.sleep(3000);
+		
+	}
+	
+	public void verify_user_can_successfull_loggedin() {
+		
+	boolean loginverify =	homepageLocatorTest3.verifyLogin.isDisplayed();
+		
+	Assert.assertTrue(loginverify);
 	}
 	
 }
--- src/test/java/utilityTest3/UtilityTest3.java
+++ src/test/java/utilityTest3/UtilityTest3.java
@@ -1,5 +1,30 @@
 package utilityTest3;
 
-public class UtilityTest3 {
+import java.io.File;
+import java.io.IOException;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class UtilityTest3 {
+	
+public static void takeMyScreenshot(WebDriver driver, String screenshot) {
+		
+		File srcfile= ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+				
+		String correntdir = System.getProperty("user.dir");
+		try {
+		FileUtils.copyFile(srcfile, new File(correntdir+"/screenShotTmavenTest3/"+screenshot+".png"));
+		} catch (IOException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
+
+		}
+
+		
 }
+
+
